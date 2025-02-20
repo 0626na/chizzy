b765a28344df450fa6267b61e5198bd501bc632e
SUKJIN HAN <0626na@gmail.com>
2025-02-20 15:50:08 +0900

    streamingPlayer 컴포넌트 생성 추후에, cloudflare를 활용한 라이브 스트리밍 부분을 구현할 예정

---
+++ app/live/components/streamPlayer.tsx
@@ -0,0 +1,11 @@
+const StreamPlayer = () => {
+  return (
+    <div className="flex flex-col bg-gray-500">
+      <div className="bg-black h-full text-white flex items-center justify-center">
+        여기에 라이브 스트리밍 영상이 나올 예정
+      </div>
+    </div>
+  );
+};
+
+export default StreamPlayer;
