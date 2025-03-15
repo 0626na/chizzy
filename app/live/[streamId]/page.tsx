e7c723d7631fad5cd0cae0abb65db52405cd5ffd
SUKJIN HAN <0626na@gmail.com>
2025-03-15 16:18:07 +0900

    feat(live/[streamId]): 특정 라이브 방송 페이지 추가

---
+++ app/live/[streamId]/page.tsx
@@ -0,0 +1,16 @@
+import Chat from "../components/chat";
+import StreamPlayer from "../components/streamPlayer";
+
+const LiveStreamingPage = ({ params }: { params: { streamId: string } }) => {
+  const { streamId } = params;
+  return (
+    <section className="flex flex-col h-screen bg-[#121212] overflow-hidden">
+      <div className="h-full flex-1 grid grid-cols-[4fr_1fr] gap-4">
+        <StreamPlayer streamId={streamId} />
+        <Chat />
+      </div>
+    </section>
+  );
+};
+
+export default LiveStreamingPage;
