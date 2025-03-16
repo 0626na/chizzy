0cc1414d9bc5e9ab6c99203e40552c3c46590a0b
SUKJIN HAN <0626na@gmail.com>
2025-03-16 19:08:55 +0900

    feat(/page (root) ): 현재 방송중인 채널을 가져와서 grid 형식으로 출력

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,42 @@
+import { getStreams } from "@/app/actions/getStreams";
 import CreateStreamButton from "@/app/components/createStreamButton";
 
-export default function Home() {
+export default async function Home() {
+  const streams = await getStreams();
+
+  if (streams.length === 0)
+    return (
+      <div className="h-full flex flex-col">
+        <h1 className="text-2xl font-bold text-center my-4">
+          현재 방송중인 채널
+        </h1>
+
+        <p className="text-center text-lg">현재 방송중인 채널이 없습니다.</p>
+        <div className="fixed bottom-4 right-4">
+          <CreateStreamButton />
+        </div>
+      </div>
+    );
   return (
-    <div>
-      <h2>Welcome to chizzy MVP</h2>
-      <p>이 페이지에는 라이브 스트리밍 영상 목록을 보일 예정</p>
-      <p>
-        Go to{" "}
-        {/* <a href="/live" style={{ color: "skyblue" }}>
-          Live Page
-        </a>{" "} */}
-        to watch the stream!
-      </p>
-      <CreateStreamButton />
+    <div className="h-full flex flex-col">
+      <h1 className="text-2xl font-bold text-center my-4">
+        현재 방송중인 채널
+      </h1>
+
+      <div className="grid grid-cols-3 lg:grid-cols-6 gap-4 p-4">
+        {streams.map((stream) => (
+          <div
+            key={stream.streamId}
+            className="bg-blue-500 p-4 rounded-lg shadow-lg flex items-center justify-center h-48"
+          >
+            <p className="text-white font-bold">{stream.title}</p>
+          </div>
+        ))}
+      </div>
+
+      <div className="fixed bottom-4 right-4">
+        <CreateStreamButton />
+      </div>
     </div>
   );
 }
